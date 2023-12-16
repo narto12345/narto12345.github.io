f1b7bdabd84e178c94d590c5f09ece1627301c5d
Nicolas <nicosan12@hotmail.com>
2023-12-16 13:52:33 -0500

    se añade el marco de presentación

---
+++ script.js
@@ -0,0 +1,6 @@
+const marcoPresentation = document.getElementById("marco-presentation");
+const buttonStart = document.getElementById("button-start");
+
+buttonStart.addEventListener("click", () => {
+  marcoPresentation.classList.add("ocultar");
+});
