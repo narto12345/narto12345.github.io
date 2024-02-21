a6a6cd702d5098d33e1e97e2cf9abddb207829dc
Nicolas <nicosan12@hotmail.com>
2024-02-20 21:58:38 -0500

    avances de experiencia en Bext y marco

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+// Funcionalidad de marco
 const marcoPresentation = document.getElementById("marco-presentation");
 const buttonStart = document.getElementById("button-start");
 
-buttonStart.addEventListener("click", () => {
-  marcoPresentation.classList.add("ocultar");
-  setTimeout(() => {
-    marcoPresentation.classList.add("desaparecer");
-  }, 1000);
-});
+try {
+  buttonStart.addEventListener("click", () => {
+    marcoPresentation.classList.add("ocultar");
+    setTimeout(() => {
+      marcoPresentation.classList.add("desaparecer");
+    }, 1000);
+  });
+} catch (ex) {
+  console.log(ex);
+}
+
+// Funcionalidad de acordeón
+const acordeon = document.getElementsByClassName("content-experience");
+
+for (let i = 0; i < acordeon.length; i++) {
+  acordeon[i].addEventListener("click", function () {
+    this.classList.toggle("activa");
+  });
+}
+
+const valores = window.location.search;
+const urlParams = new URLSearchParams(valores);
+let producto = urlParams.get("ocultar");
+
+if (producto) {
+  marcoPresentation.classList.add("desaparecer");
+}
