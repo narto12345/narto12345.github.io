e8cbd0885a51bf9533c74b882c22cdf17a8d4729
Nicolas <nicosan12@hotmail.com>
2024-02-23 19:30:02 -0500

    Se mejora visualmente para que sea cuadrado el diseño

--- js/script.js
+++ js/script.js
@@ -13,6 +13,37 @@ try {
   console.log(ex);
 }
 
+/*Funcionalidades de inicio*/
+
+// Funcionalidad para "Leer más"
+const leerMas = document.getElementById("leer-mas");
+const secondP = document.getElementById("second-p");
+let mensaje = true;
+
+try {
+  leerMas.addEventListener("click", () => {
+    secondP.classList.toggle("desaparecer");
+    if (mensaje) {
+      leerMas.innerText = "Ver menos";
+      mensaje = false;
+    } else {
+      leerMas.innerText = "Ver más";
+      mensaje = true;
+    }
+  });
+} catch (ex) {
+  console.log(ex);
+}
+
+// Funcionalidad para que al oprimir "Inicio" no se vuelva a presentar el marco
+const valores = window.location.search;
+const urlParams = new URLSearchParams(valores);
+let producto = urlParams.get("ocultar");
+
+if (producto) {
+  marcoPresentation.classList.add("desaparecer");
+}
+
 // Funcionalidad de acordeón
 const acordeon = document.getElementsByClassName("content-experience");
 
@@ -20,12 +51,4 @@ for (let i = 0; i < acordeon.length; i++) {
   acordeon[i].addEventListener("click", function () {
     this.classList.toggle("activa");
   });
-}
-
-const valores = window.location.search;
-const urlParams = new URLSearchParams(valores);
-let producto = urlParams.get("ocultar");
-
-if (producto) {
-  marcoPresentation.classList.add("desaparecer");
 }
